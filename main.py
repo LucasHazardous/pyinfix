3385fa08b9cba074df119d28d3256970f8daee23
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-12-30 12:38:53 +0100

    optimization

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ class Infix:
     COVERED = ["or", "add", "sub", "mul", 
     "matmul", "truediv", "floordiv", "mod", 
     "lshift", "rshift", "and", "xor"]
+    INITIALIZED = False
     def __init__(self, func):
         self.func = func
-        exec("".join(f"""
+        if(not Infix.INITIALIZED):
+            Infix.INITIALIZED = True
+            exec("".join(f"""
 def __r{operation}__(self, other):
     return Infix(lambda var: self.func(other, var))
 def __{operation}__(self, other):
     return self.func(other)
-        """ for operation in Infix.COVERED), globals(), method_dict := {})
-        [setattr(self.__class__, method, method_dict[method]) for method in method_dict]
+            """ for operation in Infix.COVERED), globals(), method_dict := {})
+            [setattr(self.__class__, method_name, method) for method_name, method in method_dict.items()]
